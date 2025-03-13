f86eed5935b65382c402e549bca6cbdf53e545c4
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2025-03-13 11:31:13 -0300

    Feat: Implementing interfaces in controllers

--- src/controllers/pet_lister_controller_test.py
+++ src/controllers/pet_lister_controller_test.py
@@ -17,8 +17,8 @@ def test_list_pets():
                 "type": "Pets",
                 "count": 2,
                 "attributes": [
-                    {"name": "Fluffy", "id": 4},
-                    {"name": "Buddy", "id": 47}
+                    {"name": "Fluffy", "type": "Cat", "id": 4},
+                    {"name": "Buddy",  "type": "Dog", "id": 47}
                 ]
             }
     }
